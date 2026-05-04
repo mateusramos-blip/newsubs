67209d13c397aad8889fb93dc2e844b5625c23b7
Felipe Barrios <felipe.barrios@aedb.br>
2026-05-04 12:12:31 -0300

    primeiro commit

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+const titulo = document.querySelector("h1");
+
+titulo.addEventListener("click", () => {
+  titulo.textContent = "Você clicou no título!";
+});
+
+console.log("O site carregou com sucesso!");
+
+alert("Bem-vindo ao meu site!");
+
+let visitas = localStorage.getItem("visitas");
+
+if (!visitas) {
+    visitas = 1;
+} else {
+    visitas = parseInt(visitas) + 1;
+}
+
+localStorage.setItem("visitas", visitas);
+
+document.getElementById("contador").innerText = visitas;
